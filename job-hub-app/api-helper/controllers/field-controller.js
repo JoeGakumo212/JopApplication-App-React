110271963fa7cfad323713be6c3faa269079675f
JoeGakumo <JoeGakumo212>
2023-02-05 00:09:40 +0300

    added a function for updating and deleting application

--- job-hub-app/api-helper/controllers/field-controller.js
+++ job-hub-app/api-helper/controllers/field-controller.js
@@ -1,3 +1,4 @@
+import field from "../model/field";
 import Field from "../model/field";
 // unction for getting all fields
 export const getAllFields = async (req, res) => {
@@ -55,4 +56,62 @@ export const addApplicantField = async (req, res) => {
     }
     return res.status(201).json({ field });
     
+};
+// function for updating applicants details
+
+export const updateApplication = async(req, res) => {
+    const id = req.query.id;
+    console.log(id);
+
+    const { name, email, phoneNumber, resume,
+        coverLetter, desiredPosition, skills, availability, additionalInformation } = req.body;
+    
+    if (
+        !name && name.trim() === "" &&
+        !email && email.trim() === "" &&
+        !phoneNumber && phoneNumber.trim() === "" &&
+        !resume && resume.trim() === "" &&
+        !coverLetter && coverLetter.trim() === "" &&
+        !desiredPosition && desiredPosition.trim() === "" &&
+        !skills && skills.trim() === "" &&
+        !availability && availability.trim() === "" &&
+        !additionalInformation && additionalInformation.trim() === "")
+        return res.status(422).json({ message: "Invalid Inputs" });
+    
+    let field;
+    
+
+    try {
+        field = await Field.findByIdAndUpdate(id, {
+            name, email, phoneNumber,
+            resume, coverLetter, desiredPosition,
+            skills, availability, additionalInformation
+        })
+       
+    }
+    catch (err) {
+        return console.log(err);
+        
+    }
+
+    if (!field) {
+        return res.status(500).json({ messgae: "Internal Server Error" });
+    }
+    return res.status(201).json({ message:" Congratulation, You have Successfully Updated Your Application,All The Best " });
+    
+};
+// delete function for application details
+export const deleteApplication = async (req, res) => {
+    const id = req.query.id;
+
+    let field;
+    try {
+        field = await Field.findByIdAndRemove(id);
+
+    } catch (err) {
+        return new Error(err);
+    }
+    if (!field) {
+        return res.status(500).json({ message: "Unable to Delete Application" });
+    } return res.status(200).json({ message: "Application Deleted Successfully" });
 };
---
+++ job-hub-app/pages/api/field/[id].js
@@ -0,0 +1,12 @@
+import { deleteApplication, updateApplication } from "../../../api-helper/controllers/field-controller";
+import { connectToDatabase } from "../../../api-helper/utils";
+
+export default async function handler(req, res) {
+    await connectToDatabase();
+
+    if (req.method === "PUT") {
+        return updateApplication(req, res);
+    } else if (req.method === "DELETE") {
+        return deleteApplication(req, res); 
+    }
+}
