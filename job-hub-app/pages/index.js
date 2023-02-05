e9f75c31ec634b84613826e527f8993503f9ae2e
JoeGakumo <JoeGakumo212>
2023-02-06 00:26:56 +0300

    minor charges

--- job-hub-app/pages/index.js
+++ job-hub-app/pages/index.js
@@ -11,6 +11,7 @@ function Home() {
       <Grid container >
       <Typography gutterBottom variant="h5" component="div" color={"#cc5342"} fontWeight="bold"  mt="2" padding={"20px"}>
           Job Career Hub
+          
           </Typography>
         <Grid padding={1} spacing={2} xs={1} sm={2} md={3} lg={3} height={"800px"} width={"100%"}>
          
@@ -19,13 +20,13 @@ function Home() {
             <CardContent>
           
               <Typography gutterBottom variant="h5" component="div" color={"#cc5342"} fontWeight="bold"  mt="2" textAlign="center">
-          Software Testing Jobs
+          Software  Jobs
           </Typography>
           <Typography variant="body2" color="text.secondary">
                 Discover Software Testing Jobs. Find Quick Results from Multiple Sources.
                 Get Software Testing Jobs.
                 Get Instant Quality Results
-                at iZito Now! Find Related Results Now. Get More Related Info.
+                at Job career Hub Now! Find Related Results Now. Get More Related Info.
                 Explore the Best Info Now. Discover Quality Results.
                 Powerful and Easy to Use. 100+ Qualitative Results.
                 Services: Best Results, Explore Now, New Sources, Best in Search.
@@ -46,13 +47,13 @@ function Home() {
                 <CardContent>
               
                   <Typography gutterBottom variant="h5" component="div" color={"#cc5342"} fontWeight="bold"  >
-              Software Testing Jobs
+              Software Developer Jobs
               </Typography>
               <Typography variant="body2" color="text.secondary">
                     Discover Software Testing Jobs. Find Quick Results from Multiple Sources.
                     Get Software Testing Jobs.
                     Get Instant Quality Results
-                    at iZito Now! Find Related Results Now. Get More Related Info.
+                    at Career Job Hub Now! Find Related Results Now. Get More Related Info.
                     Explore the Best Info Now. Discover Quality Results.
                     Powerful and Easy to Use. 100+ Qualitative Results.
                     Services: Best Results, Explore Now, New Sources, Best in Search.
@@ -74,13 +75,13 @@ function Home() {
                 <CardContent>
               
                   <Typography gutterBottom variant="h5" component="div" color={"#cc5342"} fontWeight="bold"  >
-              Software Testing Jobs
+              DEvOps Jobs
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                    Discover Software Testing Jobs. Find Quick Results from Multiple Sources.
+                    Discover Softwar DevOps Jobs. Find Quick Results from Multiple Sources.
                     Get Software Testing Jobs.
                     Get Instant Quality Results
-                    at iZito Now! Find Related Results Now. Get More Related Info.
+                    at Career Job Hub Now! Find Related Results Now. Get More Related Info.
                     Explore the Best Info Now. Discover Quality Results.
                     Powerful and Easy to Use. 100+ Qualitative Results.
                     Services: Best Results, Explore Now, New Sources, Best in Search.
@@ -92,7 +93,8 @@ function Home() {
                       <Button size="small">Apply Here</Button>
                     </Link>
               
-              </CardActions>
+            </CardActions>
+            
               </Card>
         </Grid>
        
